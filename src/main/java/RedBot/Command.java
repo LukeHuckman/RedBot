a7566dd216ba4eb564db4d548bf245a3c864eeb0
LukeHuckman <lukeh@outlook.my>
2020-05-06 14:51:42 +0800

    Moved commands to its own class

---
+++ src/main/java/RedBot/Command.java
@@ -0,0 +1,44 @@
+package RedBot;
+
+import java.util.List;
+import java.util.Random;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Command {
+
+    public Command(MessageReceivedEvent event, String user, String userID, String command, String[] message) {
+        Random r = new Random();
+        switch(command){
+            case "quote": //Quotes a user's post from the past 100 posts
+                List<Message> postHistory = event.getChannel().getHistoryBefore(event.getMessage(), 100).complete().getRetrievedHistory();
+                if(message[1].startsWith("<@!")){ //Quote of a paricular user
+                    for(int i=0;i<postHistory.size();i++){
+                        int random = r.nextInt(postHistory.size()-1);
+                        Message post = postHistory.get(random);
+                        if(post.getAuthor().getId().equals(message[1].substring(3, message[1].length()-1))){
+                            if(post.getAuthor().isBot()){
+                                event.getChannel().sendMessage("I can't quote bots").queue();
+                                break;
+                            }
+                            event.getChannel().sendMessage("\""+post.getContentRaw()+"\" - <@!"+post.getAuthor().getId()+">").queue();
+                            break;
+                        }
+                        event.getChannel().sendMessage("No recent posts from this user.").queue();
+                    }
+                }
+                else if(message[1].equals("random") || message[1].equals("Random")){ //Completely random quote
+                    for(int i=0;i<postHistory.size();i++){
+                        int random = r.nextInt(postHistory.size()-1);
+                        Message post = postHistory.get(random);
+                        if(post.getAuthor().isBot())
+                            continue;
+                        event.getChannel().sendMessage("\""+post.getContentRaw()+"\" - <@!"+post.getAuthor().getId()+">").queue();
+                        break;
+                    }
+                }
+                break;
+        }
+    }
+    
+}
