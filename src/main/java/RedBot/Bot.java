2b606df2ccf154a412c098936ded1ddbd24a508f
LukeHuckman <lukeh@outlook.my>
2020-10-30 05:09:17 +0800

    More cleanup

--- src/main/java/RedBot/Bot.java
+++ src/main/java/RedBot/Bot.java
@@ -146,6 +146,7 @@ public class Bot {
                         event.getChannel().sendMessage(choices[r.nextInt(choices.length)]).queue();
                 }
                 break;
+                
             case "hentai": // TODO Optimise this train wreck
                 if(message.length==1 && event.getTextChannel().isNSFW())
                     event.getChannel().sendMessage("Usage: `d.hentai <term 1> <term 2> ...`").queue();
@@ -192,6 +193,7 @@ public class Bot {
                     event.getChannel().sendMessage(hentaiEmbed).queue();
                 }
                 break;
+                
             case "mc":
             case "minecraft":
                 try { // Shows info on the Minecraft server
